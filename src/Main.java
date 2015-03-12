912b4d3ef3f6fd31dff5bf3680de955a7b05c6e1
Jacek Sikora <jaresh@wp.pl>
2015-03-12 18:12:41 +0100

    Complete algorith ClosestPoints

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,14 @@ public class Main {
 	 * @throws FileNotFoundException 
 	 */
 	public static void main(String[] args) throws FileNotFoundException {
-		FileReader fr = new FileReader("dane2.txt");
-		fr.printData();
+		FileReader fr = new FileReader("integers.txt");
 		ClosestPoints cp = new ClosestPoints();
 		cp.sortByX(fr.data);
 		cp.sortByY(fr.data);
 		cp.partXY();
-		cp.printData(fr.data);
-		cp.printParts();
-		cp.deltaHalf(cp.byX1);
-		cp.deltaHalf(cp.byX2);
-		cp.makeDeltaPart(cp.byX1, cp.byX2);
-		cp.printDeltaSpace();
-		// cp.deltaHalfLast(cp.sd1);  Need to add this
+		cp.deltaHalf();
+		cp.makeDeltaPart();
+		cp.deltaHalfLast();
 		cp.printResults();
 	}
 }
