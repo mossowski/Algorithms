0debdc3a194318b809096cb7b86ce1a53fd51795
Jacek Sikora <jaresh@wp.pl>
2015-03-28 14:54:35 +0100

    Make SParts function.

--- src/WindowQuery.java
+++ src/WindowQuery.java
@@ -1,6 +1,33 @@
+import java.awt.Point;
+import java.util.ArrayList;
+
+import javafx.scene.shape.Line;
+
 public class WindowQuery {
 
+	private ArrayList<Line> itsS = null;
+	private ArrayList<Line> itsSMid = null;
+	private ArrayList<Line> itsSLeft = null;
+	private ArrayList<Line> itsSRight = null;
+	private int itsXMed = 0;
 
-	
-	
+	public void makeSParts() {
+
+		double theXStart = 0;
+		double theXEnd = 0;
+
+		for (int i = 0; i < itsS.size(); i++) {
+
+			theXStart = itsS.get(i).getStartX();
+			theXEnd = itsS.get(i).getEndX();
+
+			if (theXEnd < itsXMed)
+				itsSLeft.add(itsS.get(i));
+			else if (theXEnd > itsXMed && theXStart < itsXMed)
+				itsSMid.add(itsS.get(i));
+			else if (theXEnd < itsXMed)
+				itsSRight.add(itsS.get(i));
+		}
+
+	}
 }
