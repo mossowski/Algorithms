0814bac56569bc7ac72dee7cd2e0375a20a6a6c4
Marcin Ossowski <lcinek18@gmail.com>
2015-04-07 22:00:56 +0200

    Add detectMaxima function

--- src/algorithms/Maxima.java
+++ src/algorithms/Maxima.java
@@ -1,8 +1,97 @@
 package algorithms;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Maxima {
 
+	public static ArrayList<Point> itsResult = null;
+
 	public Maxima() {
-		
+		itsResult = new ArrayList<Point>();
+	}
+
+	// --------------------------------------------------------------------------
+
+	/**
+	 * Detects maxima
+	 * 
+	 * @param aData
+	 * @return
+	 */
+	public ArrayList<Point> detectMaxima(ArrayList<Point> aData) {
+
+		double yMax, yMin;
+		aData = sortByX(aData);
+		itsResult.add(aData.get(0));
+		yMin = aData.get(0).getY();
+		yMax = yMin;
+
+		for (int i = 1; i < aData.size(); i++) {
+
+			double y = aData.get(i).getY();
+
+			if (y > yMax) {
+				itsResult.add(aData.get(i));
+				yMax = y;
+			}
+			if (y < yMin) {
+				itsResult.add(aData.get(i));
+				yMin = y;
+			}
+		}
+
+		int last = aData.size() - 1;
+		itsResult.add(aData.get(last));
+		yMax = yMin;
+		yMin = aData.get(last).getY();
+
+		for (int i = aData.size() - 1; i > 2; i--) {
+
+			double y = aData.get(i).getY();
+
+			if (y > yMax) {
+				itsResult.add(aData.get(i));
+				yMax = y;
+			}
+			if (y < yMin) {
+				itsResult.add(aData.get(i));
+				yMin = y;
+			}
+		}
+		return itsResult;
+	}
+
+	// --------------------------------------------------------------------------
+
+	/**
+	 * Sorts data by x
+	 * 
+	 * @param aData
+	 * @return
+	 */
+	public ArrayList<Point> sortByX(ArrayList<Point> aData) {
+		Comparator<Point> theComp = new Comparator<Point>() {
+			public int compare(Point o1, Point o2) {
+				return new Integer(o1.x).compareTo(o2.x);
+			}
+		};
+		Collections.sort(aData, theComp);
+		return aData;
+	}
+
+	// --------------------------------------------------------------------------
+
+	public void printResult(ArrayList<Point> aResult) {
+
+		if (aResult != null) {
+			System.out.println("------------------------------------------------------------");
+			for (int i = 0; i < aResult.size(); i++) {
+				System.out.println("X : " + aResult.get(i).getX() + " Y: " + aResult.get(i).getY());
+			}
+			System.out.println("------------------------------------------------------------");
+		}
 	}
 }
--- src/managers/MaximaManager.java
+++ src/managers/MaximaManager.java
@@ -1,10 +1,13 @@
 package managers;
 
+import utilities.FileReader;
 import algorithms.Maxima;
 
 public class MaximaManager {
 
 	MaximaManager() {
 		Maxima mx = new Maxima();
+		mx.detectMaxima(FileReader.itsDataPoints);
+		mx.printResult(Maxima.itsResult);
 	}
 }
